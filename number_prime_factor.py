615036df940cba39c82e7ce81b5a6c9df2063c77
Sophie <42293871+Sophie-f@users.noreply.github.com>
2018-11-25 22:53:58 +0330

    add number_prime_factor via upload
    It find  the number of prime factors.

---
+++ number_prime_factor.py
@@ -0,0 +1,20 @@
+def is_prime(x):
+    if x == 0 or x == 1:
+        return False
+    for i in range(2, int(x**0.5)+1):
+        if x%i == 0:
+            return False
+    return True        
+
+
+def count_factor(num):
+    count = 0
+    if is_prime(num):
+        count = 1
+    else:
+        for factor in range(2, num//2 + 1):
+            if is_prime(factor) and (num % factor == 0):
+                count += 1
+    return count            
+n = int(input('Enter the number '))
+print(count_factor(n))    
