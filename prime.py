af31a06f843c7006a92afa0dbb9cf7753d24f985
Sophie <42293871+Sophie-f@users.noreply.github.com>
2018-11-25 13:38:26 +0330

    add prime file
    Enter a number (n). It shows you the prime numbers before n.

---
+++ prime.py
@@ -0,0 +1,13 @@
+def is_prime(n):
+    if n == 1 or n == 0:    
+        return False
+    for i in range(2,int(n**0.5)+1):
+        if n%i == 0:
+            return False
+    return True        
+count = 0
+for i in range(1,100):
+    if is_prime(i):
+        print (i)
+        count = count+1
+print(count)
