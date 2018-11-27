78fc9d823ecd765f2c7b25f9421253dde246fc78
Sophie <42293871+Sophie-f@users.noreply.github.com>
2018-11-27 19:29:54 +0330

    add file
    It reverse every word in a string.

---
+++ reverse_word.py
@@ -0,0 +1,11 @@
+def reverse(s):
+    new_s =' '
+    j = None
+    for i in range(len(s)):
+        if s[i] == " ":
+            new_s += s[i-1:j:-1] + ' '
+            j = i
+    new_s += s[i:j:-1]        
+    return new_s 
+
+print(reverse(input('Enter string ')))    
